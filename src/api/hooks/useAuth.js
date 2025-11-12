69d8e0a23e082610af492144297e451847f14b4e
Hayeon-Jung <jhy000714@gmail.com>
2025-11-12 17:16:03 +0900

    feat: checkEmail, checkNickname, register

--- src/api/hooks/useAuth.js
+++ src/api/hooks/useAuth.js
@@ -6,6 +6,64 @@ import { useDispatch } from "react-redux";
 import { logout } from "@/store/userSlice";
 
 // 실제 API 대체용
+const mockRegister = async (data) => {
+  await new Promise((res) => setTimeout(res, 500));
+  console.log("회원가입 요청 데이터:", data);
+
+  if (data.email === "test@test.com") {
+    const error = new Error("이미 존재하는 이메일입니다.");
+    error.code = 201;
+    error.response = {
+      data: {
+        code: 201,
+        message: "이미 존재하는 이메일입니다.",
+      },
+    };
+    throw error;
+  }
+
+  return {
+    message: "회원가입이 완료되었습니다.",
+    userProfile: { username: data.nickname, points: 0 },
+  };
+};
+
+const mockCheckEmail = async (email) => {
+  await new Promise((res) => setTimeout(res, 500));
+  console.log("이메일 중복 확인 요청:", email);
+
+  if (email === "test@test.com") {
+    const error = new Error("이미 존재하는 이메일입니다.");
+    error.code = 409;
+    error.response = {
+      data: { code: 409, message: "이미 존재하는 이메일입니다." },
+    };
+    throw error;
+  }
+  return {
+    code: 200,
+    message: "사용 가능한 이메일입니다.",
+  };
+};
+
+const mockCheckNickname = async (nickname) => {
+  await new Promise((res) => setTimeout(res, 500));
+  console.log("이메일 중복 확인 요청:", nickname);
+
+  if (nickname === "닉네임") {
+    const error = new Error("이미 존재하는 닉네임입니다.");
+    error.code = 409;
+    error.response = {
+      data: { code: 409, message: "이미 존재하는 닉네임입니다." },
+    };
+    throw error;
+  }
+  return {
+    code: 200,
+    message: "사용 가능한 닉네임입니다.",
+  };
+};
+
 const mockLogin = async (data) => {
   await new Promise((res) => setTimeout(res, 1000));
   if (data.email === "test@test.com" && data.password === "1234") {
@@ -20,6 +78,21 @@ const mockLogin = async (data) => {
 
 // 추후 API 사용 예제
 
+// const realRegister = async (data) => {
+//   const res = await api.post("/auth/register", data);
+//   return res.data;
+// }
+
+// const checkEmail = async (email) => {
+//   const res = await api.post("/auth/check-email", {email});
+//   return res.data;
+// }
+
+// const checkNickname = async (nickname) => {
+//   const res = await api.post("/auth/check-nickname", {nickname});
+//   return res.data;
+// }
+
 // const realLogin = async (data) => {
 //   const res = await api.post("/auth/login", data);
 //   return res.data; // { accessToken, user }
@@ -28,6 +101,41 @@ const mockLogin = async (data) => {
 export const useAuth = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const registerMutation = useMutation({
+    mutationFn: mockRegister,
+    onSuccess: (res) => {
+      navigate("/login");
+    },
+    onError: (err) => {
+      console.error("회원가입 실패 :", err.message);
+      return err;
+    },
+  });
+
+  const checkEmailMutation = useMutation({
+    mutationFn: mockCheckEmail,
+    onSuccess: (res) => {
+      console.log(res);
+    },
+    onError: (err) => {
+      alert(
+        err.response?.data?.message || "이메일 확인 중 오류가 발생했습니다.",
+      );
+    },
+  });
+
+  const checkNicknameMutation = useMutation({
+    mutationFn: mockCheckNickname,
+    onSuccess: (res) => {
+      console.log(res);
+    },
+    onError: (err) => {
+      alert(
+        err.response?.data?.message || "이메일 확인 중 오류가 발생했습니다.",
+      );
+    },
+  });
 
   const loginMutation = useMutation({
     mutationFn: mockLogin,
@@ -53,5 +161,11 @@ export const useAuth = () => {
     navigate("/");
   };
 
-  return { loginMutation, handleLogout };
+  return {
+    checkEmailMutation,
+    checkNicknameMutation,
+    registerMutation,
+    loginMutation,
+    handleLogout,
+  };
 };
