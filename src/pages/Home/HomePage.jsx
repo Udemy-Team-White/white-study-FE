3c1a629aab1614355f3f1ccf90313f7caf5e98de
Hayeon-Jung <jhy000714@gmail.com>
2025-11-14 18:35:58 +0900

    test: Sample data - StudyBox & Pagination

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../api/hooks/useAuth";
+import Pagination from "../../components/common/Pagination";
+import { useState } from "react";
+import StudyBox from "./components/StudyBox";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
 
 const HomePage = () => {
   const navigate = useNavigate();
   const { handleLogout } = useAuth();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 13;
 
   return (
     <>
@@ -11,6 +24,16 @@ const HomePage = () => {
       <div onClick={() => navigate("/register")}>회원가입</div>
       <div onClick={() => navigate("/login")}>로그인 페이지</div>
       <div onClick={() => navigate("/my")}>마이 페이지</div>
+      <Container>
+        <StudyBox />
+        <StudyBox />
+        <StudyBox />
+      </Container>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
     </>
   );
 };
