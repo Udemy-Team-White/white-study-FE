35ee66754ef8fa59ad07ba8d7043e8c201d9c7f2
Hayeon-Jung <jhy000714@gmail.com>
2025-12-02 13:22:49 +0900

    feat: add report Comp

---
+++ src/pages/MyStudyDash/ReportComp/Report.jsx
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+import { Lilac0, White } from "../../../styles/colors";
+import { LilacButtonStyle } from "../../../components/common/button";
+import { useMediaQuery } from "react-responsive";
+import { useGetReports } from "../../../api/queries/useGetReports";
+import { Body, BodyBold } from "../../../styles/fonts";
+import { formatDate } from "../../../utils/formatDate";
+import { useState } from "react";
+import Pagination from "../../../components/common/Pagination";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleIsModalOpen } from "../../../store/modalSlice";
+import Modal from "../../../components/common/Modal";
+import ReportDetail from "./component/ReportDetail";
+
+const reportsData = {
+  reports: [
+    {
+      reportId: 0,
+      authorUsername: "string (작성자 닉네임)",
+      contentSnippet: "string (내용 100자 요약)",
+      createdAt: new Date("2024-12-11"),
+    },
+    {
+      reportId: 1,
+      authorUsername: "string (작성자 닉네임)",
+      contentSnippet: "string (내용 100자 요약)",
+      createdAt: new Date("2024-12-11"),
+    },
+  ],
+  pageInfo: {
+    page: 1,
+    size: 1,
+    totalPages: 1,
+    totalElements: 2,
+  },
+};
+
+const Container = styled.div`
+  background-color: ${Lilac0};
+  max-width: 1080px;
+  margin: auto;
+  padding: 24px;
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+`;
+
+const GridLine = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 24px;
+  margin-bottom: 12px;
+  width: 100%;
+  @media (min-width: 767px) {
+    grid-template-columns: 3fr 1fr;
+  }
+`;
+
+const Button = styled.button`
+  ${LilacButtonStyle}
+`;
+
+const ReportBox = styled.div`
+  background-color: ${White};
+  border-radius: 16px;
+  padding: 20px 24px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  cursor: pointer;
+`;
+
+const ReportTitle = styled.div`
+  ${BodyBold}
+  cursor: pointer;
+`;
+
+const ReportDate = styled.div`
+  ${Body}
+  cursor: pointer;
+`;
+
+const Report = ({ studyId, setActiveTab }) => {
+  const isDesktop = useMediaQuery({ minWidth: 767 });
+
+  const { reportsData } = useGetReports(studyId);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = reportsData?.pageInfo?.totalPages;
+
+  const dispatch = useDispatch();
+  const isModalOpen = useSelector((state) => state.modalUi.isModalOpen);
+
+  const [selectedReportId, setSelectedReportId] = useState(null);
+
+  const openModal = (reportId) => {
+    dispatch(toggleIsModalOpen());
+    setSelectedReportId(reportId);
+  };
+
+  return (
+    <>
+      {isModalOpen && (
+        <Modal bgColor={White} reportId={selectedReportId}>
+          <ReportDetail reportId={selectedReportId} />
+        </Modal>
+      )}
+
+      <Container>
+        <GridLine>
+          {isDesktop && <div></div>}
+
+          <Button type="button" onClick={() => setActiveTab(6)}>
+            보고서 작성하기
+          </Button>
+        </GridLine>
+
+        {reportsData?.reports?.map((report) => (
+          <ReportBox
+            key={report.reportId}
+            onClick={() => openModal(report.reportId)}
+          >
+            <ReportTitle>{report?.contentSnippet}</ReportTitle>
+            <ReportDate>{formatDate(report?.createdAt)}</ReportDate>
+          </ReportBox>
+        ))}
+
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages || 1}
+          onPageChange={setCurrentPage}
+        />
+      </Container>
+    </>
+  );
+};
+
+export default Report;
---
+++ src/pages/MyStudyDash/ReportComp/component/ReportDetail.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import { Heading5Bold } from "../../../../styles/fonts";
+import { useGetReport } from "../../../../api/queries/useGetReport";
+import { Lilac0, StudyLilac } from "../../../../styles/colors";
+import { FaRegLightbulb } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { useMediaQuery } from "react-responsive";
+import {
+  GrayButtonStyle,
+  LilacButtonStyle,
+} from "../../../../components/common/button";
+import { setIsModalOpen } from "../../../../store/modalSlice";
+
+const reportData = {
+  subject: "제목!",
+  summary: "한 줄 요약!",
+  content: "<p>내용이에요.</p>",
+};
+
+const Title = styled.div`
+  ${Heading5Bold}
+  border-bottom: 1px solid ${StudyLilac};
+  padding-bottom: 20px;
+`;
+
+const SummaryBox = styled.div`
+  background-color: ${Lilac0};
+  margin: 24px 0;
+  border-radius: 16px;
+  padding: 24px;
+  display: flex;
+`;
+
+const LightIcon = styled(FaRegLightbulb)`
+  height: 24px;
+  color: ${StudyLilac};
+  margin-right: 16px;
+`;
+
+const GridBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 24px;
+
+  @media (min-width: 767px) {
+    grid-template-columns: 2fr 1fr 1fr;
+  }
+`;
+
+const CancleButton = styled.button`
+  ${GrayButtonStyle}
+`;
+
+const EditButton = styled.button`
+  ${LilacButtonStyle}
+`;
+
+const ReportDetail = ({ reportId }) => {
+  const { reportData } = useGetReport(reportId);
+
+  const dispatch = useDispatch();
+
+  const isDesktop = useMediaQuery({ minWidth: 767 });
+
+  return (
+    <>
+      <Title>{reportData?.subject}</Title>
+      <SummaryBox>
+        <LightIcon />
+        {reportData?.summary}
+      </SummaryBox>
+      <div dangerouslySetInnerHTML={{ __html: reportData?.content }} />
+      <GridBox>
+        {isDesktop && <div />}
+        <EditButton>수정</EditButton>
+        <CancleButton onClick={() => dispatch(setIsModalOpen(false))}>
+          닫기
+        </CancleButton>
+      </GridBox>
+    </>
+  );
+};
+
+export default ReportDetail;
